0844d55522c4f20f50a52281e9cd32b6450e56f0
Manuel Paniagua <manuel.paniagua84@gmail.com>
2019-03-30 10:47:32 -0700

    add active class at start

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $('#sidebarCollapse').toggleClass('active');
+        $('#sidebar, #sidebarCollapse, #content').toggleClass('active');
+        // $('#sidebarCollapse').toggleClass('active');
+        // $('#content').toggleClass('active');
     });
 });
